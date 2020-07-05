b9878ccce24129b7caefb45b46d6aee54f37e89d
Addie Morrison <developer@0xADD1E.me>
2020-07-05 14:47:40 -0500

    Allow users to set their own npm-like executables Namely, `yarn`

--- README.md
+++ README.md
@@ -239,6 +239,7 @@ use actix_web_static_files::NpmBuild;
 
 fn main() {
     NpmBuild::new("./web")
+        .executable("yarn")
         .install().unwrap()
         .run("build").unwrap()
         .target("./web/dist")
--- src/lib.rs
+++ src/lib.rs
@@ -241,6 +241,7 @@ use actix_web_static_files::NpmBuild;
 
 fn main() {
     NpmBuild::new("./web")
+        .executable("yarn")
         .install().unwrap()
         .run("build").unwrap()
         .target("./web/dist")
