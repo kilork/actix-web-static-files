3d997860f79fafa403352eb09d0085c74a2b2188
Alexander Korolev <alexander.korolev.germany@gmail.com>
2025-08-21 23:04:35 +0200

    added examples to release

--- release.sh
+++ release.sh
@@ -7,7 +7,22 @@ if [ "${RELEASE_TYPE}" != "current" ]; then
   cargo set-version --bump ${RELEASE_TYPE}
 fi
 VERSION=`cargo pkgid | cut -d"#" -f2`
+export CRATE="actix-web-static-files"
 export CRATE_RUST_MAJOR_VERSION=`echo ${VERSION} | cut -d"." -f1,2`
+if [[ "${RELEASE_TYPE}" != "patch" && "${RELEASE_TYPE}" != "current" ]]; then
+    pushd ../${CRATE}-examples
+    git checkout main
+    git pull
+    cargo upgrade -p ${CRATE}@${CRATE_RUST_MAJOR_VERSION}
+    cargo update
+    cargo build
+    git add .
+    git commit -m"${CRATE} version ${CRATE_RUST_MAJOR_VERSION}"
+    git branch v${CRATE_RUST_MAJOR_VERSION}
+    git push
+    git push origin v${CRATE_RUST_MAJOR_VERSION}
+    popd
+fi
 handlebars-magic templates .
 git add .
 git commit -m"Release ${VERSION}"
